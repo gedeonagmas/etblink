bbf2e69caebac0b5b7999e8c0fc35440b15ec9e4
gedeonagmas <gedeonagmas2580@gmail.com>
2024-03-24 14:52:14 +0300

    form updated

--- client/src/pages/dashboard/Profile.jsx
+++ client/src/pages/dashboard/Profile.jsx
@@ -484,8 +484,110 @@ const Profile = () => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
+        <div className="flex w-full border rounded-lg p-2.5 items-center gap-6">
+          <p className="font-bold">Tuesday</p>
+          From
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          To
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div className="flex w-full border rounded-lg p-2.5 items-center gap-6">
+          <p className="font-bold">Wednesday</p>
+          From
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          To
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div className="flex w-full border rounded-lg p-2.5 items-center gap-6">
+          <p className="font-bold">Thursday</p>
+          From
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          To
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div className="flex w-full border rounded-lg p-2.5 items-center gap-6">
+          <p className="font-bold">Friday</p>
+          From
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          To
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div className="flex w-full border rounded-lg p-2.5 items-center gap-6">
+          <p className="font-bold">Saturday</p>
+          From
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          To
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div className="flex w-full border rounded-lg p-2.5 items-center gap-6">
+          <p className="font-bold">Monday</p>
+          From
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          To
+          <input
+            type="time"
+            name=""
+            id=""
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
       </div>
-      <div className="mb-5">
+      {/* <div className="mb-5">
         <label
           for="name"
           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -520,7 +622,7 @@ const Profile = () => {
           />
           <button className="p-2 add rounded-lg text-white bg-main">Add</button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
