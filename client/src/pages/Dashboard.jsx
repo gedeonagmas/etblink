98b1f0fe9830a681bc970a44c7671cb4dc361088
gedeonagmas <gedeonagmas2580@gmail.com>
2024-06-14 03:07:46 +0300

    updated

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1850,7 +1850,7 @@ const Dashboard = () => {
 
             <li>
               <a
-                href="/dashboard/notification"
+                href="/dashboard/notifications"
                 class="flex items-center p-2 text-gray-500 hover:text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
